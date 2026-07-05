3d6a237468bf043832f7e4144c177c5e8feb7ee7
Mishti Abrol <mishtiabrol100@gmail.com>
2026-07-05 17:32:21 +0530

    UPDATE

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://mediqueue-5q7s.onrender.com/api"
+  baseURL: "https://mediqueuebooking.onrender.com"
 });
 
 export default API;
